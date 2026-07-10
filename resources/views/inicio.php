74e28291c26f70584670009cf2be111f61d80e3e
franconex <ismaelfrancoruiz3007@gmail.com>
2026-07-10 10:05:28 -0400

    Integrantes del grupo

---
+++ resources/views/inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrantes</title>
+</head>
+<body>
+
+    <h1>integrantes del grupo</h1>
+    <p>1. Ismael Franco Ruiz</p>
+    <p>2. Luis Gabriel Mendieta Orellana (cuñado) </p>
+    <p>3. Lucas Herrera Becerra</p>
+    <p>4. Diana Camila Ayala</p>
+</body>
+</html>
