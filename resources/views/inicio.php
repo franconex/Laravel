8f92bb5966ee595ba889b745286c475f92026cc4
franconex <ismaelfrancoruiz3007@gmail.com>
2026-07-09 21:34:31 -0400

    Primer commit

---
+++ resources/views/inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>integrantes del grupo</title>
+</head>
+<body>
+    <h1>Integrantes del grupo</h1>
+    <p>1. Ismael Franco Ruiz</p>
+    <p>2. Luis Gabriel Mendieta Orellana (cuñado) </p>
+    <p>3. Lucas Herrera Becerra</p>
+</body>
+</html>
